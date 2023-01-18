879f6e31f263b32aac0111c6570456dea6c93442
GDI <administrator@gdi-service.de>
2023-01-18 14:18:36 +0100

    beta 15-3.3

--- 15-3.3/build/sources/20_pg_cron.sql
+++ 15-3.3/build/sources/20_pg_cron.sql
@@ -1,4 +1,4 @@
+\echo CREATE EXTENSION pg_cron;
 \c postgres;
 CREATE EXTENSION IF NOT EXISTS pg_cron;
 SELECT cron.schedule('30 3 * * 6', $$DELETE FROM events WHERE event_time < now() - interval '1 year'$$);
-SELECT cron.schedule('0 2 * * 6', 'VACUUM');
--- 15-3.3/build/sources/40_pg_track_settings.sql
+++ 15-3.3/build/sources/40_pg_track_settings.sql
@@ -1,3 +1,4 @@
+\echo CREATE EXTENSION pg_track_settings
 \c postgres;
 CREATE EXTENSION IF NOT EXISTS pg_track_settings;
 SELECT cron.schedule('0 0 * * 6', 'select pg_track_settings_snapshot()');
